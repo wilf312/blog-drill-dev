966e65748dcd833b76eb3c81260387c3d629fe04
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-10-27 00:37:10 +0900

    add ssr styled-components match className

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {DocumentContext} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class _document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) => props => {
+            return sheet.collectStyles(<App {...props} />)
+          }
+        })
+      }
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
