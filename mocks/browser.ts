1f2aa115275edebc4c4810453232c0c06ac4eec8
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-12-31 17:41:40 +0900

    replace

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -4,6 +4,18 @@ import { handlers } from './handlers'
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers)
 
+export type Msw = {
+  worker: typeof worker,
+  rest: typeof rest
+}
+
+declare global {
+  interface Window {
+    msw: Msw
+  }
+}
+
+
 window.msw = {
   worker,
   rest,
