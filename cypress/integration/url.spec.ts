b024bb7869b6247e0dbbfccb7519c71ee62ea242
OkadaGenya <g.okada.wilf+github@gmail.com>
2021-01-06 20:54:44 +0900

    テストのコメントアウト

--- cypress/integration/url.spec.ts
+++ cypress/integration/url.spec.ts
@@ -1,29 +1,30 @@
 /// <reference types="cypress" />
 
 import list from '../url.json'
-// context('Actions', () => {
-//   it('ページアクセス', () => {
-//     list.forEach(page => {
-//       cy.visit(`http://localhost:11111${page.url}`)
-//       cy.contains(page.title)
-//     })
-//   })
-// })
-
-context('mswテスト', () => {
-  it('https://mswjs.io/docs/api/setup-worker/use', () => {
-    cy.visit('http://localhost:11111/pages/test_github')
-    cy.window().then(({msw}: Window) => {
-      // Reference global instances set in "src/mocks.js".
-      const { worker, rest } = msw
-      worker.use(
-        rest.get('https://my.backend/book', (req, res, ctx) => {
-          return res(ctx.json({
-            title: 'aaaaaaaaa',
-          }))
-        }),
-      )
-      cy.contains('aaaaaaaaa')
+context('Actions', () => {
+  it('ページアクセス', () => {
+    list.forEach(page => {
+      cy.visit(`http://localhost:11111${page.url}`)
+      cy.contains(page.title)
     })
   })
 })
+
+// APIのモックテスト
+// context('mswテスト', () => {
+//   it('https://mswjs.io/docs/api/setup-worker/use', () => {
+//     cy.visit('http://localhost:11111/pages/test_github')
+//     cy.window().then(({msw}: Window) => {
+//       // Reference global instances set in "src/mocks.js".
+//       const { worker, rest } = msw
+//       worker.use(
+//         rest.get('https://my.backend/book', (req, res, ctx) => {
+//           return res(ctx.json({
+//             title: 'aaaaaaaaa',
+//           }))
+//         }),
+//       )
+//       cy.contains('aaaaaaaaa')
+//     })
+//   })
+// })
